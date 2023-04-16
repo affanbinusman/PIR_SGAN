6e09f9ba1a72f5f7f7f60c28ec9f66927d5edd51
Chinmay Bhale <40037215+chinmaybhale@users.noreply.github.com>
2023-04-16 07:57:13 -0700

    Sending new data to backend and handling chance of zero collisions

--- sim.js
+++ sim.js
@@ -1,32 +1,43 @@
 var persons = [];
 var robot;
 
-var trajectories = [];
+var rtrajs = [];
+var rpath = [];
+
+var ptrajs = [];
 var predictions = [];
 
 var timestep = 0;
 const numberOfPeople = 2;
 
 function getNewPredictions() {
-	const data = {"data": trajectories};
+
+	const data = {
+		"numberOfPeople": numberOfPeople,
+		"ptrajs": ptrajs, 
+		"rtrajs": rtrajs
+	};
+	
 	fetch("http://localhost:8000/get_preds", {
-			method: 'POST',
-			headers: {
-					'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
+		method: 'POST',
+		headers: {
+				'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
 	})
 	.then(response => {
-			if (response.ok) {
-					return response.json();
-			}
-			else {
-					throw new Error("barf!");
-			}
+		if (response.ok) {
+				return response.json();
+		}
+		else {
+				throw new Error("barf!");
+		}
 	})
 	.then(data => {
-			predictions = data.preds;
-			loop();
+		predictions = data.preds;
+		if (data.preds == 0) 
+			ptrajs = [];
+		loop();
 	});
 }
 
@@ -47,12 +58,16 @@ function setup() {
 									randomGaussian(0), randomGaussian(0)));
 	}
 
-	// persons.push(new Person(75, 75, 1, 1));
-	// persons.push(new Person(125, 125, -1, -1));
 }
 
 function draw() {
 	background(0);
+
+	robot.display()
+	rtrajs.push([timestep, 0, robot.posX, robot.posY]);
+
+	if (rtrajs.length > 8)
+		rtrajs.shift();
 
 	for (let i = 0; i < numberOfPeople; i++) {
 		if (predictions.length != 0) {
@@ -63,17 +78,17 @@ function draw() {
 		}
 		persons[i].display();
 		
-		trajectories.push([timestep, i + 1, persons[i].posX, persons[i].posY]);
+		ptrajs.push([timestep, i + 1, persons[i].posX, persons[i].posY]);
 
-		if (trajectories.length > (persons.length * 8)) {
-			trajectories.shift();
+		if (ptrajs.length > (persons.length * 8)) {
+			ptrajs.shift();
 		}
 	}
 
 	predictions.shift();
 	console.log(predictions)
 
-	if (trajectories.length >= (persons.length * 8) &&
+	if (ptrajs.length >= (numberOfPeople * 8) &&
 		predictions.length == 0) {
 		getNewPredictions();
 		noLoop();
